2232e51ff662e69ee4e67fef90cb762092d7d540
Christopher Crnjak <christopher.crnjak@aol.de>
2024-02-06 22:56:18 +0100

    added some html and added bootstrap

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let url = 'https://pokeapi.co/api/v2/pokemon/gengar';
     let response = await fetch(url);
     currentPokemon = await response.json();
-    console.log('Loaded pokemon ', currentPokemon);
+    console.log('Loaded pokemon ', currentPokemon['name'], currentPokemon);
 
     renderPokemonInfo();
 }
@@ -16,8 +16,19 @@ function renderPokemonInfo() {
     document.getElementById('pokemonName').innerHTML = capitalizeFirstLetter(pokeName);
     document.getElementById('id').innerHTML = `# ${pokeId}`;
     document.getElementById('pokemon_image').src = pokeImg;
+    renderPokeTypes();
 }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function renderPokeTypes() {
+    let pokeTypesJSON = currentPokemon['types'];
+    for (let i = 0; i < pokeTypesJSON.length; i++) {
+        const pokeType = pokeTypesJSON[i]['type']['name'];
+        document.getElementById('pokeType').innerHTML += /*html*/`
+            <div>${pokeType}</div>
+        `;
+    }
 }
